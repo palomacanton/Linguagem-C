e3b3b775432b8739eadcc387aba913b88fe839ab
palomacanton <palomacanton04@gmail.com>
2024-03-25 22:29:47 -0300

    struct aninhada 2

--- Atividades/struct_aninhada2.c
+++ Atividades/struct_aninhada2.c
@@ -1,30 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct departamento {
-    int cod;
-    char descricao[30];
-} Departamento;
-
-typedef struct cargo {
-    int cod;
-    char descricao[30];
-} Cargo;
-
-typedef struct funcionario {
-    int cod;
-    char nome[30];
-    float salario;
-    Departamento depto;
-    Cargo cargo;
-} Funcionario;
-
-int main(void) {
-    // Vetor de funcionarios
-    Funcionario nome_vetor_struct[10]; // Supondo que seja um vetor de 10 elementos
-    
-    // Acessando um elemento do vetor e imprimindo seu nome
-    printf("\nTEXTO %s ", nome_vetor_struct[0].nome); //vai imprimir um símbolo estranho pq ñ tem nada no vetor
-    
-    return 0;
-}
+    #include <stdio.h> 
+	#include <stdlib.h>
+	#include <string.h>
+	#include <locale.h>
+		
+	typedef struct {
+		int dia;
+		int mes;
+		int ano;
+	} Data;
+		
+	typedef struct { 
+		int codigo;
+		char nome[200];
+		Data datNasc;
+	} Aluno;
+	Aluno aluno;
+		
+	int main() { 
+		setlocale(LC_ALL,"portuguese"); //configuração de localização
+		aluno.codigo = 0;
+		strcpy(aluno.nome, "NULL"); // serve para atribuir "NULL" para a variével "nome", digamos inicializar a variável.
+		aluno.datNasc.dia = 0;
+		aluno.datNasc.mes = 0;
+		aluno.datNasc.ano = 0;
+		printf(" \n O código do aluno é: %d ", aluno.codigo);
+		printf(" \n O nome do aluno é: %s ", aluno.nome);
+		printf(" \n A data de nascimento do aluno é: %d / %d / %d ", aluno.datNasc.dia, aluno.datNasc.mes, aluno.datNasc.ano);
+		printf(" \n \n");
+		printf(" Digite o código do aluno: ");
+		scanf("%d%*c", &aluno.codigo);
+		printf(" Digite o nome do aluno: ");
+		scanf("%s%*c", &aluno.nome);
+		printf(" Digite o dia do nascimento do aluno: ");
+		scanf("%d%*c", &aluno.datNasc.dia);
+		printf(" Digite o mês do nascimento do aluno: ");
+		scanf("%d%*c", &aluno.datNasc.mes);
+		printf(" Digite o ano do nascimento do aluno: ");
+		scanf("%d%*c", &aluno.datNasc.ano);
+		printf(" \n O código do aluno é: %d ", aluno.codigo);
+		printf(" \n O nome do aluno é: %s ", aluno.nome);
+		printf(" \n A data de nascimento do aluno é: %d / %d / %d ", aluno.datNasc.dia, aluno.datNasc.mes, aluno.datNasc.ano);
+		printf(" \n \n");
+		system("pause"); //dependendo do sistema, serve para esperar o usuário digitar algo antes de continuar
+		return(0);
+	}												
