c1172ac74ad8e99558a1ff73c2072bb59d8cd1c3
palomacanton <palomacanton04@gmail.com>
2024-03-26 15:04:35 -0300

    detalhe array_struct

--- Atividades/array_struct.c
+++ Atividades/array_struct.c
@@ -12,31 +12,31 @@ int main() { //"int main(){}" é mais indicado do que "void main(){}"
     int i;
 	
     for(i = 0; i < 1; i++) { //repetição das 3 perguntas x vezes
-        printf(" Digite nome do aluno: ");
+        printf("\nDigite nome do aluno: ");
         scanf("%s%*c", aluno_nota[i].nome);
-        printf(" Digite a disciplina do aluno: ");
+        printf("Digite a disciplina do aluno: ");
         scanf("%s%*c", aluno_nota[i].disciplina);
-        printf(" Digite o nota do aluno: ");
+        printf("Digite o nota do aluno: ");
         scanf("%f%*c", &aluno_nota[i].nota); //"%*c"->caractere lido será salvo e o indesejado(ex: "\n") será ignorado e não armazenará na variável(limpa buffer). "%f"->float
     }
     for(i=0; i < 1; i++) { //imprime a lista de perguntas repetidas acima
-        printf(" \n O nome do aluno é: %s ", aluno_nota[i].nome);
-        printf(" \n A disciplina do aluno é: %s ", aluno_nota[i].disciplina);
-        printf(" \n A nota do aluno é: %.2f ", aluno_nota[i].nota);
+        printf(" \nO nome do aluno é: %s ", aluno_nota[i].nome);
+        printf(" \nA disciplina do aluno é: %s ", aluno_nota[i].disciplina);
+        printf(" \nA nota do aluno é: %.2f ", aluno_nota[i].nota);
     }
     int posicao;
     char nome[30];
-    printf(" \n Digite um nome para saber se consta na lista: ");
+    printf("\nDigite um nome para saber se consta na lista: ");
     scanf("%s%*c", nome);
     
     for(i=0; i< 10; i++) {
         if(strcmp(nome, aluno_nota[i].nome)== 0) {
-            printf("\n Registro encontrado! ");
+            printf("\nRegistro encontrado! ");
             posicao = i;
             break;
         } else {
             posicao = -1;
-            printf(" \n Registro não encontrado! ");
+            printf("\nRegistro não encontrado! ");
             break;
         }
     }
