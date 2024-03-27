b11949009bc1c74415892a37c1620809cd962ec9
palomacanton <palomacanton04@gmail.com>
2024-03-26 22:15:05 -0300

    criação de procedimento.c

---
+++ Atividades/procedimento.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//Ao se ignorar o valor de retorno de uma função e, para esta chamada, ela será equivalente a um procedimento.
+void imprime (int numero) {
+        printf ("Número %d\n", numero);
+    }	
+int main(){
+    imprime(2);
+
+    return 0;
+}
+    	
